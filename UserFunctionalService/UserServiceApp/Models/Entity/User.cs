410f115b9b8e31d12eb176977e2ecdee69878cec
asaf <128684083+Asaf-prog@users.noreply.github.com>
2024-04-27 21:05:05 +0300

    connect and configure a db on my local machine, next step is to design the db and the connections between the entities :)

---
+++ UserFunctionalService/UserServiceApp/Models/Entity/User.cs
@@ -0,0 +1,7 @@
+public class User
+{
+     public int UserId { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public int PhoneNumber { get; set; }
+}
---
+++ UserFunctionalService/UserServiceApp/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace UserServiceApp.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
