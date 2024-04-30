65936d66aa0711f5e1128f78e12019548218fc2c
asaf <128684083+Asaf-prog@users.noreply.github.com>
2024-04-30 16:24:08 +0300

    add LoginService to solution

--- Dto/DTO/MedicationDTO.cs
+++ Dto/DTO/MedicationDTO.cs
@@ -1,12 +1,70 @@
 using System;
 namespace MrPill.DTOs.DTOs;
 
+using System;
+
 public class MedicationDTO
 {
     public int Id { get; set; }
-    public string Name { get; set; }
-    public string Description { get; set; }
+    public string? Name { get; set; }
+    public string? Description { get; set; }
     public DateTime? Validity { get; set; }
     public int UserId { get; set; }
     public int MedicationRepoId { get; set; }
+
+    public class MedicationDTOBuilder
+    {
+        private MedicationDTO _medicationDTO;
+
+        public MedicationDTOBuilder()
+        {
+            _medicationDTO = new MedicationDTO();
+        }
+
+        public MedicationDTOBuilder WithId(int id)
+        {
+            _medicationDTO.Id = id;
+            return this;
+        }
+
+        public MedicationDTOBuilder WithName(string? name)
+        {
+            _medicationDTO.Name = name;
+            return this;
+        }
+
+        public MedicationDTOBuilder WithDescription(string? description)
+        {
+            _medicationDTO.Description = description;
+            return this;
+        }
+
+        public MedicationDTOBuilder WithValidity(DateTime? validity)
+        {
+            _medicationDTO.Validity = validity;
+            return this;
+        }
+
+        public MedicationDTOBuilder WithUserId(int userId)
+        {
+            _medicationDTO.UserId = userId;
+            return this;
+        }
+
+        public MedicationDTOBuilder WithMedicationRepoId(int medicationRepoId)
+        {
+            _medicationDTO.MedicationRepoId = medicationRepoId;
+            return this;
+        }
+
+        public MedicationDTO Build()
+        {
+            return _medicationDTO;
+        }
+    }
+
+    public static MedicationDTOBuilder Builder()
+    {
+        return new MedicationDTOBuilder();
+    }
 }
--- Dto/DTO/UserDTO.cs
+++ Dto/DTO/UserDTO.cs
@@ -3,8 +3,59 @@
 public class UserDTO
 {
     public int UserId { get; set; }
-    public string FirstName { get; set; }
-    public string LastName { get; set; }
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
     public int PhoneNumber { get; set; }
     public int HouseId { get; set; }
+
+    public class UserDTOBuilder
+    {
+        private UserDTO _userDTO;
+
+        public UserDTOBuilder()
+        {
+            _userDTO = new UserDTO();
+        }
+
+        public UserDTOBuilder WithUserId(int userId)
+        {
+            _userDTO.UserId = userId;
+            return this;
+        }
+
+        public UserDTOBuilder WithFirstName(string firstName)
+        {
+            _userDTO.FirstName = firstName;
+            return this;
+        }
+
+        public UserDTOBuilder WithLastName(string lastName)
+        {
+            _userDTO.LastName = lastName;
+            return this;
+        }
+
+        public UserDTOBuilder WithPhoneNumber(int phoneNumber)
+        {
+            _userDTO.PhoneNumber = phoneNumber;
+            return this;
+        }
+
+        public UserDTOBuilder WithHouseId(int houseId)
+        {
+            _userDTO.HouseId = houseId;
+            return this;
+        }
+
+        public UserDTO Build()
+        {
+            return _userDTO;
+        }
+    }
+
+    public static UserDTOBuilder Builder()
+    {
+        return new UserDTOBuilder();
+    }
 }
+
