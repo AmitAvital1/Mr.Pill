34f6879e4e274b99fe1ca69ca30c6450e3ad9f58
AmitAvital1 <amitavital21@gmail.com>
2024-09-04 17:01:25 +0300

    Added cloud and build configurations

--- Client/MrPill/RequestHandler.ts
+++ Client/MrPill/RequestHandler.ts
@@ -3,7 +3,7 @@ import DataHandler from "./DataHandler";
 
 
 // change if need
-const BASE_URL = "http://10.100.102.17:"
+const BASE_URL = "http://20.217.66.65:"
 const SERVER_AND_CLIENT_ON_SAME_MACHINE = true;
 
 // do not change
