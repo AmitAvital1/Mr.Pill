70263e0c3c28efe122430c3d8d1b586bfb351374
Orenik <orensh2@mta.ac.il>
2024-09-08 22:38:48 +0300

    bump mrpill v1

--- Client/MrPill/RequestHandler.ts
+++ Client/MrPill/RequestHandler.ts
@@ -3,7 +3,7 @@ import DataHandler from "./DataHandler";
 
 // change if need 
 const BASE_URL = "http://20.217.66.65:"
-const SERVER_AND_CLIENT_ON_SAME_MACHINE = true;
+const SERVER_AND_CLIENT_ON_SAME_MACHINE = false;
 const COOLDOWN_PERIOD = 1500 // time in MS user has to wait between requests of the same type.
 
 // currently unused
